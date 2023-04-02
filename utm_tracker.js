595eea46a9ee62ab1a984ef1e2f2978b13d3a0a6
Aidan <adssoccer1@gmail.com>
2023-04-02 14:38:00 -0400

    add utm tracker-js log statements

--- utm_tracker.js
+++ utm_tracker.js
@@ -1,11 +1,14 @@
 console.log("utm_tracker loaded successfully");
 
 function getUTMParameters() {
+    console.log("getUTMParameters called ");
     const urlParams = new URLSearchParams(window.location.search);
     const utmParameters = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content'];
     const utmData = {};
   
     utmParameters.forEach(param => {
+        console.log("getUTMParameters -> ", param);
+
       if (urlParams.has(param)) {
         utmData[param] = urlParams.get(param);
       }
@@ -15,10 +18,15 @@ function getUTMParameters() {
   }
   
   const utmData = getUTMParameters();
+  console.log("utmData:  ", utmData);
 
   function setCookie(name, value, days) {
+    console.log("setCookie called ");
+
     const expires = new Date(Date.now() + days * 864e5).toUTCString();
     document.cookie = `${name}=${encodeURIComponent(value)}; expires=${expires}; path=/; SameSite=Lax`;
+    console.log("setCookie set " );
+
   }
   
   function getCookie(name) {
@@ -26,12 +34,18 @@ function getUTMParameters() {
   }
   
   Object.entries(utmData).forEach(([key, value]) => {
+    console.log("Objec.entries called ", key , " ", value);
+
     if (!getCookie(key)) {
+        console.log("set a cook ", key , " ", value);
+
       setCookie(key, value, 30);
     }
   });
 
   async function updateCartAttributes(attributes) {
+    console.log("updateCartAttributes called ", attributes);
+
     try {
       const response = await fetch('/cart/update.js', {
         method: 'POST',
